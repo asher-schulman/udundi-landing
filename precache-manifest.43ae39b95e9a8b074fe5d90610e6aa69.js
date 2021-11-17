5e03ec21b4d07e4fa3997e30f18458ca78bccf04
asher8346 <asher.schulman@gmail.com>
2021-11-17 07:52:37 -0600

    deploy

---
+++ precache-manifest.43ae39b95e9a8b074fe5d90610e6aa69.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22ae0abc389bdeb99c8b",
+    "url": "/udundi-landing/css/app.22837429.css"
+  },
+  {
+    "revision": "458dd9f2e615324c378434ba6d47d7c5",
+    "url": "/udundi-landing/img/Facebook.458dd9f2.svg"
+  },
+  {
+    "revision": "2a0335f85dcf235e669b31ad668d2e25",
+    "url": "/udundi-landing/img/Instagram.2a0335f8.svg"
+  },
+  {
+    "revision": "0b63ee7f9e16f865a4797ce449a0e72c",
+    "url": "/udundi-landing/img/Plus.0b63ee7f.svg"
+  },
+  {
+    "revision": "37236e97d90f2aba8a7ee2ef37b7ab40",
+    "url": "/udundi-landing/img/new-background.37236e97.jpg"
+  },
+  {
+    "revision": "ca200c043892dc1331ffcde2cce4b78c",
+    "url": "/udundi-landing/index.html"
+  },
+  {
+    "revision": "36f28426a495e81a704f",
+    "url": "/udundi-landing/js/about.dd1731ef.js"
+  },
+  {
+    "revision": "22ae0abc389bdeb99c8b",
+    "url": "/udundi-landing/js/app.ffdb03ed.js"
+  },
+  {
+    "revision": "80f4d2a35b1983b53ef4",
+    "url": "/udundi-landing/js/chunk-vendors.bc2bfd1f.js"
+  },
+  {
+    "revision": "44944b45df9e84f162ba7c114b31776f",
+    "url": "/udundi-landing/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/udundi-landing/robots.txt"
+  }
+]);
